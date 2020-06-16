6df73640e93f32e5816d74144e3e4f8c26215619
cjspindel <cjspindel@gmail.com>
2020-06-16 15:12:14 -0500

    Add MMM, alkane body moiety

--- cgnp_patchy/cgnp_patchy.py
+++ cgnp_patchy/cgnp_patchy.py
@@ -82,6 +82,14 @@ class MME(mb.Compound):
         self.add(mb.Particle(name="_MME"))
         self.add(mb.Port(anchor=self[0], orientation=[0, 1, 0], separation=0.1), 'up')
 
+class MMM(mb.Compound):
+    """ Coarse-grained alkane bead containing a CH2-CH2-CH2 group """
+    def __init__(self):
+        super(MMM, self).__init__()
+        self.add(mb.Particle(name='_MMM'))
+        self.add(mb.Port(anchor=self[0], orientation=[0, 1, 0], separation=0.15), 'up')
+        self.add(mb.Port(anchor=self[0], orientation=[0, -1, 0], separation=0.15), 'down')
+
 class cgnp_patchy(mb.Compound):
     """
     Example class that would go in your recipe.
