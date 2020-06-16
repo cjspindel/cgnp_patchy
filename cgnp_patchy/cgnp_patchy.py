805984810f282f9058af511e554a1287310c4f59
cjspindel <cjspindel@gmail.com>
2020-06-16 15:06:44 -0500

    Add MME, alkane cap moiety

--- cgnp_patchy/cgnp_patchy.py
+++ cgnp_patchy/cgnp_patchy.py
@@ -75,6 +75,12 @@ class Nanoparticle(mb.Compound):
 
         return np.any(dists < 2.0 * radius)
 
+class MME(mb.Compound):
+    """ Coarse-grained alkane bead containing a CH2-CH2-CH3 group """
+    def __init__(self):
+        super(MME, self).__init__()
+        self.add(mb.Particle(name="_MME"))
+        self.add(mb.Port(anchor=self[0], orientation=[0, 1, 0], separation=0.1), 'up')
 
 class cgnp_patchy(mb.Compound):
     """
