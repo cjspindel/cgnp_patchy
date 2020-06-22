a6a7a40e04d9a4535346a2574e34ab2b854352a1
cjspindel <cjspindel@gmail.com>
2020-06-22 16:26:56 -0500

    Add BaseTest class with basic pytest fixtures

--- cgnp_patchy/tests/test_cgnp_patchy.py
+++ cgnp_patchy/tests/test_cgnp_patchy.py
@@ -7,6 +7,20 @@ import cgnp_patchy
 import pytest
 import sys
 import mbuild as mb
+import numpy as np
+
+class BaseTest:
+    @pytest.fixture
+    def Core(self):
+        return Nanoparticle(radius=2.5, bead_diameter=0.6) 
+    
+    @pytest.fixture
+    def Alkane(self):
+        return CGAlkane()
+
+    @pytest.fixture
+    def GraftedNanoparticle(self):
+        return cgnp_patchy(radius=2.5, bead_diameter=0.6, chain_density=2.0)
 
 def test_cgnp_patchy_imported():
     """ Sample test, will always pass so long as import statement worked """
