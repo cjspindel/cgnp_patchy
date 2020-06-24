d290720887278e97bd7fc78a36b0d48c13b98482
cjspindel <cjspindel@gmail.com>
2020-06-24 14:57:03 -0500

    Test mbuild's SpherePattern

--- cgnp_patchy/tests/test_cgnp_patterns.py
+++ cgnp_patchy/tests/test_cgnp_patterns.py
@@ -4,6 +4,13 @@ Unit and regression tests for coating patterns in the cgnp_patchy package.
 
 import pytest
 import mbuild as mb
+import numpy as np
+
+def test_mbuild_sphere_pattern():
+    radius = 2.5
+    point_density = 3.0 
+    pattern = mb.SpherePattern(int(point_density*4.0*np.pi*radius**2.0))
+    assert len(pattern.points) == 235
 
 def test_bipolar_pattern():
     from cgnp_patchy.lib.patterns import BipolarPattern
