3f0677c003dbbc1de508ae128ed90d4ea5d7391c
cjspindel <cjspindel@gmail.com>
2020-06-29 10:56:07 -0500

    Test if number of points in pattern plus points removed equals full isotropic pattern

--- cgnp_patchy/tests/test_cgnp_patterns.py
+++ cgnp_patchy/tests/test_cgnp_patterns.py
@@ -24,24 +24,28 @@ class TestPatterns(BaseTest):
         pattern = BipolarPattern(radius=2.5, chain_density=3.0, fractional_sa=0.2)
         assert len(pattern.points) == 189
         assert count_patch_points(pattern, 2.5, 3.0) == 46
+        assert len(pattern.points) + count_patch_points(pattern, 2.5, 3.0) == len(IsotropicPattern.points)
 
     def test_equatorial_pattern(self, IsotropicPattern):
         from cgnp_patchy.lib.patterns import EquatorialPattern
         pattern = EquatorialPattern(radius=2.5, chain_density=3.0, fractional_sa=0.2)
         assert len(pattern.points) == 188 
         assert count_patch_points(pattern, 2.5, 3.0) == 47 
+        assert len(pattern.points) + count_patch_points(pattern, 2.5, 3.0) == len(IsotropicPattern.points)
 
     def test_polar_pattern(self, IsotropicPattern):
         from cgnp_patchy.lib.patterns import PolarPattern
         pattern = PolarPattern(radius=2.5, chain_density=3.0, fractional_sa=0.2)
         assert len(pattern.points) == 188 
         assert count_patch_points(pattern, 2.5, 3.0) == 47
+        assert len(pattern.points) + count_patch_points(pattern, 2.5, 3.0) == len(IsotropicPattern.points)
 
     def test_cube_pattern(self, IsotropicPattern):
         from cgnp_patchy.lib.patterns import CubePattern
         pattern = CubePattern(radius=2.5, chain_density=3.0, fractional_sa=0.2)
         assert len(pattern.points) == 163
         assert count_patch_points(pattern, 2.5, 3.0) == 72
+        assert len(pattern.points) + count_patch_points(pattern, 2.5, 3.0) == len(IsotropicPattern.points)
 
     def test_random_pattern(self, IsotropicPattern):
         from cgnp_patchy.lib.patterns import RandomPattern
@@ -53,16 +57,19 @@ class TestPatterns(BaseTest):
         pattern = RingPattern(radius=2.5, chain_density=3.0, fractional_sa=0.2)
         assert len(pattern.points) == 181 
         assert count_patch_points(pattern, 2.5, 3.0) == 54
+        assert len(pattern.points) + count_patch_points(pattern, 2.5, 3.0) == len(IsotropicPattern.points)
 
     def test_square_pattern(self, IsotropicPattern):
         from cgnp_patchy.lib.patterns import SquarePattern
         pattern = SquarePattern(radius=2.5, chain_density=3.0, fractional_sa=0.2)
         assert len(pattern.points) == 185
         assert count_patch_points(pattern, 2.5, 3.0) == 50
+        assert len(pattern.points) + count_patch_points(pattern, 2.5, 3.0) == len(IsotropicPattern.points)
 
     def test_tetrahedral_pattern(self, IsotropicPattern):
         from cgnp_patchy.lib.patterns import TetrahedralPattern
         pattern = TetrahedralPattern(radius=2.5, chain_density=3.0, fractional_sa=0.2)
         assert len(pattern.points) == 163
         assert count_patch_points(pattern, 2.5, 3.0) == 72
+        assert len(pattern.points) + count_patch_points(pattern, 2.5, 3.0) == len(IsotropicPattern.points)
 
